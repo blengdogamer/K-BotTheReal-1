33845dffaf59a452f34eb5f4616c6deca32d3641
iiKaix <46973236+iiKaix@users.noreply.github.com>
2019-04-24 22:52:18 +0100

    Update botbc.js

--- botbc.js
+++ botbc.js
@@ -27,7 +27,58 @@ client.on('message', message => {
   }
 });
 
-
+//k&role
+bot.on('message', msg => {
+  if(msg.author.bot) return
+  if(msg.content.startsWith(prefix + 'role')) {
+  let params = msg.content.slice(prefix.length).trim().split(/ +/g);
+  if(!params[0]) return msg.channel.send(`**syntax: ${prefix}role <all/humans/bots/@user> <name role/@role>`);
+if(params[0] === 'all') {
+ if(!params[1]) return msg.channel.send(`**منشن الرتبة او اكتب اسمها \n syntax: ${prefix}role all <@role / name role>**`)
+     let role = msg.mentions.roles.first() || msg.guild.roles.find(r =>  r.name.toLowerCase().startsWith(params[1].toLowerCase()))
+   if(!role) return msg.channel.send(`**لم استطع ايجاد هذه الرتبة**`)
+ msg.guild.members.forEach(m => {
+if(m.roles.some(r => r.id == role.id)) return
+     m.addRole(role)
+ })
+ msg.channel.send(`**تم إعطاك الكل رتبة e @${role.name}**`);
+} else if(params[0] === 'bots') {
+ if(!params[1]) return msg.channel.send(`**منشن الرتبة او اكتب اسمها \n syntax: ${prefix}role bots <@role / name role>**`)
+     let role = msg.mentions.roles.first() || msg.guild.roles.find(r =>  r.name.toLowerCase().startsWith(params[1].toLowerCase()))
+   if(!role) return msg.channel.send(`**لم استطع ايجاد هذه الرتبة**`)
+ let bots = msg.guild.members.filter(m => m.user.bot)
+ bots.forEach(bot => {
+   if(bot.roles.some(r => r.id == role.id)) return
+   bot.addRole(role)
+ })
+ msg.channel.send(`**تم ، إعطاك كل البوتات رتبة  @${role.name}**`);
+} else if(params[0] === 'humans') {
+ if(!params[1]) return msg.channel.send(`**منشن الرتبة او اكتب اسمها \n syntax: ${prefix}role humans <@role / name role>**`)
+     let role = msg.mentions.roles.first() || msg.guild.roles.find(r =>  r.name.toLowerCase().startsWith(params[1].toLowerCase()))
+   if(!role) return msg.channel.send(`**لم استطع ايجاد هذه الرتبة**`)
+   let humans = msg.guild.members.filter(m => !m.user.bot)
+   humans.forEach(h => {
+     if(h.roles.some(r => r.id == role.id)) return
+     h.addRole(role)
+   })
+   msg.channel.send(`**تـم ، لقد تم إعطاء الأعضاء  @${role.name}**`);
+}else {
+     if(!params[1]) return msg.channel.send(`**منشن الرتبة او اكتب اسمها \n syntax: ${prefix}role @user <@role / name role>**`)
+     let role = msg.mentions.roles.first() || msg.guild.roles.find(r =>  r.name.toLowerCase().startsWith(params[1].toLowerCase()))
+     if(!role) return msg.channel.send(`**لم استطع ايجاد هذه الرتبة**`)
+     let userID = params[0].slice(2 , -1)
+     let user = msg.guild.members.get(userID)
+     if(!user) return
+     user.addRole(role)
+     msg.channel.send(`**تـم إعطآء  ${user} @${role.name}**`)
+ 
+   }
+ 
+ 
+ }
+ 
+ 
+})
 //ticket
 client.on("message", (message) => {
    if (message.content.startsWith("k&new")) {     
@@ -1481,6 +1532,8 @@ k&id | لمعرفة بعض المعلومات عنك في السيرفر**
 و no لعدم الإرسال
 
 k&clear | لمسح الشات (500 رسالة)
+
+k&role all / humans / bots / @user || لإعطاء الأعضاء \ البوتات \ الكل \ شخص رتبة معينة
 
 k&mute |لوضع الميوت على شخص معين 
 
